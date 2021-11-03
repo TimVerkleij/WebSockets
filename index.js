9178c81ee00ed7548c8c1fa1463bf36048d54256
Tim Verkleij <tim.verkleij@student.hu.nl>
2021-11-03 16:32:10 +0100

    Add serverside time tracking (not finished)

--- index.js
+++ index.js
@@ -12,13 +12,37 @@ app.use(Express.static('public/'));
 
 const { WebSocketServer } = require('ws')
 
+
+let timers = {}
+
 const wss = new WebSocketServer({ port: 6969 });
 
 wss.on('connection', function connection(ws) {
 	ws.on('message', function message(data) {
 		let response = JSON.parse(data)
-		console.log(response)
+		// console.log(response)
+
+		let id = response.clientID
+
+		switch (response.action) {
+			case "Start":
+				startTime = new Date().getTime()
+				stopTime = null
+				timers[`${id}`] = { startTime, stopTime }
+				break
+			case "Stop":
+				stopTime = new Date().getTime()
+				timers[`${id}`].stopTime = stopTime
+				break
+		}
+
+		console.log(timers)
+
+
+
 	});
+
+	ws.send(uuid())
 });
 
 wss.on('close', function close() {
@@ -27,3 +51,10 @@ wss.on('close', function close() {
 
 console.log("listening on port: " + process.env.PORT)
 app.listen(process.env.PORT)
+
+function uuid() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
