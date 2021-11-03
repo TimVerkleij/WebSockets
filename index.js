f0e2c657bc4126b5fce1e2a691bf6ec2963b1e67
Tim Verkleij <tim.verkleij@student.hu.nl>
2021-11-03 09:56:37 +0100

    Working websockets

--- index.js
+++ index.js
@@ -12,39 +12,14 @@ app.use(Express.static('public/'));
 
 const { WebSocketServer } = require('ws')
 
-const wss = new WebSocketServer({
-  port: 6969,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      // See zlib defaults.
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-    // Other options settable:
-    clientNoContextTakeover: true, // Defaults to negotiated value.
-    serverNoContextTakeover: true, // Defaults to negotiated value.
-    serverMaxWindowBits: 10, // Defaults to negotiated value.
-    // Below options specified as default values.
-    concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024 // Size (in bytes) below which messages
-    // should not be compressed if context takeover is disabled.
-  }
-})
+const wss = new WebSocketServer({ port: 6969 });
 
-const WebSocket = require('ws')
-
-const ws = new WebSocket('ws://localhost:6969/idk')
-
-ws.on('open', function open() {
-    ws.send('something');
-});
-
-ws.on('message', function message(data) {
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
     console.log('received: %s', data);
+  });
+
+  ws.send('something');
 });
 
 console.log("listening on port: " + process.env.PORT)
