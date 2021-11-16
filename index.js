560768325e0b3a56bb2e3c6902bad618e4717348
Tim Verkleij <tim.verkleij@student.hu.nl>
2021-11-16 13:41:52 +0100

    idk

--- index.js
+++ index.js
@@ -28,21 +28,36 @@ wss.on('connection', function connection(ws) {
 			case "Start":
 				startTime = new Date().getTime()
 				stopTime = null
-				timers[`${id}`] = { startTime, stopTime }
+				totalTime = 0
+				timers[`${id}`] = { startTime, stopTime, totalTime }
+				setInterval(sendTimerData, 1000, ws, id, timers[`${id}`])
 				break
 			case "Stop":
 				stopTime = new Date().getTime()
 				timers[`${id}`].stopTime = stopTime
+				totalTime =  stopTime - timers[`${id}`].startTime
+				timers[`${id}`].totalTime += totalTime
+				break
+			case "Pause":
+				pauseTime = new Date().getTime()
+				totalTime = pauseTime - timers[`${id}`].startTime
+				timers[`${id}`].totalTime += totalTime
+				break
+			case "Resume":
+				startTime = new Date().getTime()
+				timers[`${id}`].startTime = startTime
 				break
 		}
 
 		console.log(timers)
-
+		let timerData = {clientID: id, timer: timers[`${id}`], running: true}
+		ws.send(JSON.stringify(timerData))
 
 
 	});
 
-	ws.send(uuid())
+	let data = {clientID: uuid(), running: false}
+	ws.send(JSON.stringify(data))
 });
 
 wss.on('close', function close() {
@@ -51,6 +66,12 @@ wss.on('close', function close() {
 
 console.log("listening on port: " + process.env.PORT)
 app.listen(process.env.PORT)
+
+function sendTimerData(ws, clientID, timer) {
+	let timerData = {clientID, timer, running: true}
+	timer.currentTime = new Date().getTime() - timer.startTime
+	ws.send(JSON.stringify(timerData))
+}
 
 function uuid() {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
